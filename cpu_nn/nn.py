Fixed bugs to make the network work end to end. Add test cases. Details: 1. Casting data structures into np.float 2. Broadcasting during addition did not take care of (1,1) vectors 3. Weird numpy problem. Discovered that numpy.transpose does not change the memory layout. This caused indexing problem in CUDA C. 4. Added coursera (Andrew Ng) test cases.
Now the network works, however it is super slow (orders of magnitude
slower than CPU version).

cpu_nn/nn.py
@@ -361,7 +361,7 @@ if __name__ == "__main__":
     # ----- Check _one_layer_backward_propagation()
     print("----- Check _one_layer_backward_propagation()")
 
-    dAL = [[-0.41675785, -0.05626683]]
+    dAL = np.array([[-0.41675785, -0.05626683]])
     linear_activation_cache = ((np.array([[-2.1361961, 1.64027081],
                                           [-1.79343559, -0.84174737],
                                           [0.50288142, -1.24528809]]),
@@ -402,6 +402,8 @@ if __name__ == "__main__":
                 np.array([[-1.62328545]])),
                np.array([[0.64667545, -0.35627076]])))
 
+    expected_result = [[-0.39202432 -0.13325855 -0.04601089]]
+
     print("\n\n", network_obj.backward_propagation(AL, Y_assess, caches)[1][1])
 
     # ----- Check update_parameters()
@@ -429,4 +431,15 @@ if __name__ == "__main__":
     network_obj.weights = W
     network_obj.biases = b
     network_obj.update_parameters(grads, 0.1)
+
+    """
+     Expected Result:
+        W:[array([[-0.5956207 , -0.09991781, -2.14584585,  1.82662008],
+       [-1.76569677, -0.80627147,  0.51115557, -1.18258802],
+       [-1.0535704 , -0.86128581,  0.68284051,  2.20374577]]), array([[-0.55569196,  0.0354055 ,  1.32964895]])], 
+        b:[array([[-0.04659241],
+       [-1.28888276],
+       [ 0.53405496]]), array([[-0.84610769]])]
+
+    """
     print("\n\nW:{}, \nb:{}".format(network_obj.weights, network_obj.biases))

gpu_nn/gpu_helper.py
@@ -17,7 +17,7 @@ from pycuda.compiler import SourceModule
 BLOCK_DIMS = (32, 32, 1)
 GRID_DIMS = (1, 1, 1)
 
-DIR_PATH = os.path.realpath("..")
+DIR_PATH = os.path.realpath(".")
 sys.path.append(DIR_PATH)
 print("dir vdswv", DIR_PATH)
 
@@ -27,8 +27,12 @@ def matrix_multiplication(matrix1, matrix2, debug=0):
 
     :param matrix1:
     :param matrix2:
+    :param debug:
     :return:
     """
+
+    matrix1 = matrix1.astype(np.float32)
+    matrix2 = matrix2.astype(np.float32)
     matrix1_nrows = np.int32(matrix1.shape[0])
     matrix1_ncols = np.int32(matrix1.shape[1])
     matrix2_nrows = np.int32(matrix2.shape[0])
@@ -39,14 +43,12 @@ def matrix_multiplication(matrix1, matrix2, debug=0):
 
     output_matrix = np.empty((matrix1_nrows, matrix2_ncols)).astype(np.float32)
 
-    print(f"Matrix1: {matrix1}, Matrix2: {matrix2}")
-
     matrix1_gpu = driver.mem_alloc(matrix1.nbytes)
     matrix2_gpu = driver.mem_alloc(matrix2.nbytes)
     output_matrix_gpu = driver.mem_alloc(output_matrix.nbytes)
 
     driver.memcpy_htod(matrix1_gpu, matrix1)
-    driver.memcpy_htod(matrix2_gpu, matrix1)
+    driver.memcpy_htod(matrix2_gpu, matrix2)
 
     kernel_file = os.path.join(DIR_PATH, "gpu_nn/kernels/matrix_multiplication.c")
     ker_code = SourceModule(open(kernel_file, 'r').read())
@@ -61,10 +63,12 @@ def matrix_multiplication(matrix1, matrix2, debug=0):
            grid=GRID_DIMS)
 
     driver.memcpy_dtoh(output_matrix, output_matrix_gpu)
+
     return output_matrix
 
 
 def scalar_matrix_addition(scalar, matrix, debug=0):
+    matrix = matrix.astype(np.float32)
     output_matrix_cpu = np.empty(matrix.shape).astype(np.float32)
 
     matrix_gpu = driver.mem_alloc(matrix.nbytes)
@@ -122,20 +126,30 @@ def matrix_matrix_addition(matrix1, matrix2):
     return output_matrix
 
 
-def vector_matrix_addition(vector, matrix):
+def vector_matrix_addition(vector, matrix, debug=0):
     """
     Addition of a vector and a matrix. Vector is broadcasted.
     :param matrix:
     :param vector:
+    :param debug:
     :return:
     """
 
-    if vector.shape[0] == 1:
+    if vector.shape[0] == 1 and vector.shape[1] == 1:
+        # For a vector with just one element
+        return scalar_matrix_addition(vector[0, 0], matrix)
+    elif vector.shape[0] == 1:
+        # Row wise vector
         broadcast_dimension = 0
     elif vector.shape[1] == 1:
+        # Column wise vector
         broadcast_dimension = 1
     else:
         raise ValueError("Vector is not broadcastable. {} != {}".format(matrix.shape, vector.shape))
+
+
+    matrix = matrix.astype(np.float32)
+    vector = vector.astype(np.float32)
 
     output_matrix = np.empty(matrix.shape).astype(np.float32)
 
@@ -154,7 +168,7 @@ def vector_matrix_addition(vector, matrix):
     vector_matrix_add(matrix_gpu, vector_gpu, output_matrix_gpu,
                       np.int32(matrix.shape[0]), np.int32(matrix.shape[1]),
                       np.int32(broadcast_dimension),
-                      np.int32(0),
+                      np.int32(debug),
                       block=BLOCK_DIMS,
                       grid=GRID_DIMS)
 
@@ -163,13 +177,16 @@ def vector_matrix_addition(vector, matrix):
     return output_matrix
 
 
-def scalar_matrix_multiplication(scalar, matrix):
+def scalar_matrix_multiplication(scalar, matrix, debug=0):
     """
 
     :param scalar:
     :param matrix:
+    :param debug:
     :return:
     """
+
+    matrix = matrix.astype(np.float32)
     output_matrix_cpu = np.empty(matrix.shape).astype(np.float32)
 
     matrix_gpu = driver.mem_alloc(matrix.nbytes)
@@ -184,7 +201,7 @@ def scalar_matrix_multiplication(scalar, matrix):
 
     matrix_scalar_multiply(matrix_gpu, np.float32(scalar), output_matrix_gpu,
                            np.int32(matrix.shape[0]), np.int32(matrix.shape[1]),
-                           np.int32(0),
+                           np.int32(debug),
                            block=BLOCK_DIMS,
                            grid=GRID_DIMS)
 
@@ -199,6 +216,8 @@ def element_wise_exponent(matrix, base=math.e):
     :param base:
     :return:
     """
+
+    matrix = matrix.astype(np.float32)
     output_matrix_cpu = np.empty(matrix.shape).astype(np.float32)
 
     matrix_gpu = driver.mem_alloc(matrix.nbytes)
@@ -223,6 +242,8 @@ def element_wise_exponent(matrix, base=math.e):
 
 
 def element_wise_reciprocal(matrix, numerator=1.0):
+    matrix = matrix.astype(np.float32)
+
     output_matrix_cpu = np.empty(matrix.shape).astype(np.float32)
 
     numpy_answer_cpu = np.divide(numerator, matrix)
@@ -260,6 +281,8 @@ def matrix_element_multiplication(matrix1, matrix2):
     if matrix1.shape != matrix2.shape:
         raise ValueError("Cannot perform addition. Shapes don't match. {} != {}".format(matrix1.shape, matrix2.shape))
 
+    matrix1 = matrix1.astype(np.float32)
+    matrix2 = matrix2.astype(np.float32)
     output_matrix = np.empty(matrix1.shape).astype(np.float32)
 
     matrix1_gpu = driver.mem_alloc(matrix1.nbytes)
@@ -285,16 +308,19 @@ def matrix_element_multiplication(matrix1, matrix2):
     return output_matrix
 
 
-def matrix_subtraction(matrix1, matrix2):
+def matrix_subtraction(matrix1, matrix2, debug=0):
     """
         Addition of 2 2d matrices
         :param matrix1:
         :param matrix2:
+        :param debug:
         :return:
         """
     if matrix1.shape != matrix2.shape:
         raise ValueError("Cannot perform addition. Shapes don't match. {} != {}".format(matrix1.shape, matrix2.shape))
 
+    matrix1 = matrix1.astype(np.float32)
+    matrix2 = matrix2.astype(np.float32)
     output_matrix = np.empty(matrix1.shape).astype(np.float32)
 
     matrix1_gpu = driver.mem_alloc(matrix1.nbytes)
@@ -311,7 +337,7 @@ def matrix_subtraction(matrix1, matrix2):
 
     matrix_subtract(matrix1_gpu, matrix2_gpu, output_matrix_gpu,
                     np.int32(matrix1.shape[0]), np.int32(matrix1.shape[1]),
-                    np.int32(0),
+                    np.int32(debug),
                     block=BLOCK_DIMS,
                     grid=GRID_DIMS)
 
@@ -320,16 +346,19 @@ def matrix_subtraction(matrix1, matrix2):
     return output_matrix
 
 
-def matrix_element_division(matrix1, matrix2):
+def matrix_element_division(matrix1, matrix2, debug=0):
     """
         Division of 2 2d matrices
         :param matrix1:
         :param matrix2:
+        :param debug:
         :return:
         """
     if matrix1.shape != matrix2.shape:
         raise ValueError("Cannot perform addition. Shapes don't match. {} != {}".format(matrix1.shape, matrix2.shape))
 
+    matrix1 = matrix1.astype(np.float32)
+    matrix2 = matrix2.astype(np.float32)
     output_matrix = np.empty(matrix1.shape).astype(np.float32)
 
     matrix1_gpu = driver.mem_alloc(matrix1.nbytes)
@@ -342,13 +371,13 @@ def matrix_element_division(matrix1, matrix2):
     kernel_file = os.path.join(DIR_PATH, "gpu_nn/kernels/matrix_element_division.c")
     ker_code = SourceModule(open(kernel_file, 'r').read())
 
-    matrix_subtract = ker_code.get_function("matrix_element_division")
+    matrix_element_divide = ker_code.get_function("matrix_element_division")
 
-    matrix_subtract(matrix1_gpu, matrix2_gpu, output_matrix_gpu,
-                    np.int32(matrix1.shape[0]), np.int32(matrix1.shape[1]),
-                    np.int32(0),
-                    block=BLOCK_DIMS,
-                    grid=GRID_DIMS)
+    matrix_element_divide(matrix1_gpu, matrix2_gpu, output_matrix_gpu,
+                          np.int32(matrix1.shape[0]), np.int32(matrix1.shape[1]),
+                          np.int32(debug),
+                          block=BLOCK_DIMS,
+                          grid=GRID_DIMS)
 
     driver.memcpy_dtoh(output_matrix, output_matrix_gpu)
 